473fe50f53cb76c796c433e9ace82e2ad2f88c8e
Yinkfu Bazil N. <164238680+YinkfuNdi@users.noreply.github.com>
2025-06-30 02:03:57 -0700

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import threading
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "✅ Bot is running!"
+
+def run():
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+def keep_alive():
+    t = threading.Thread(target=run)
+    t.start()
